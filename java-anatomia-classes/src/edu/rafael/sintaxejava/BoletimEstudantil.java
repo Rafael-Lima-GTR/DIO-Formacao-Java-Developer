cf6d5d06e8fd01911698d6f411f5a03972468581
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-02 14:53:27 -0300

    Criado os pacotes e feito as divisões dos exemplos das aulas por assunto pertinente.

---
+++ java-anatomia-classes/src/edu/rafael/sintaxejava/BoletimEstudantil.java
@@ -0,0 +1,30 @@
+package edu.rafael.sintaxejava;
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+
+        /*
+         * Exemplo do código não indentado
+         * 
+         * int mediaFinal = 7;
+         * 
+         * if(mediaFinal<6)
+         * System.out.println("REPROVADO");
+         * else if(mediaFinal==6)
+         * System.out.println("PROVA MINERVA");
+         * else
+         * System.out.println("APROVADO");
+         */
+
+        // Código indentado
+        int mediaFinal = 5;
+
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+
+    }
+}
