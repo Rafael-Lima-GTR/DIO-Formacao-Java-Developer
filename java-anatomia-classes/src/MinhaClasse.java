a43da83e51fa0a20a6ce1bfb4b0b5a0a9750ee18
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-01 22:27:44 -0300

    01_Anatomia das Classes: exemplo 02

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,14 @@ public class MinhaClasse {
     
     public static void main(String [] args){
         
-        System.out.print("Olá turma, sejam bem-vindos");
+        String primeiroNome = "Rafael";
+        String segundoNome = "Sousa";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método" + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
