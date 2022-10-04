2a30d555175df79ff1c679133a898904c18ceb1e
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-04 09:00:09 -0300

    03_Operadores: exemplo 02

--- java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
+++ java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
@@ -4,17 +4,42 @@ public class Operadores {
 
     public static void main(String[] args) {
 
-        String concatenacao = "?";
+        // // EXEMPLO DE CONCATENAÇÃO
+        // String concatenacao = "?";
 
-        concatenacao = 1 + 1 + 1 + "1";
+        // concatenacao = 1 + 1 + 1 + "1";
+        // System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + 1;
+        // concatenacao = 1 + "1" + 1 + 1;
+        // System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + "1";
+        // concatenacao = 1 + "1" + 1 + "1";
+        // System.out.println(concatenacao);
 
-        concatenacao = "1" + 1 + 1 + 1;
+        // concatenacao = "1" + 1 + 1 + 1;
+        // System.out.println(concatenacao);
 
-        concatenacao = "1" + (1 + 1 + 1);
+        // concatenacao = "1" + (1 + 1 + 1);
+        // System.out.println(concatenacao);
+
+        // EXEMPLO DE OPERADOR TERNÁRIO
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*
+         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         */
+
+        // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a == b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
     }
 
 }
