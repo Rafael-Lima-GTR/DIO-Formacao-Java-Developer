52eb6a2d78d62430f021d8653292cd3a0cc0c3a3
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-06 22:16:24 -0300

    03_Operadores: exemplo 03

--- java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
+++ java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
@@ -22,24 +22,48 @@ public class Operadores {
         // concatenacao = "1" + (1 + 1 + 1);
         // System.out.println(concatenacao);
 
-        // EXEMPLO DE OPERADOR TERNÁRIO
-        int a, b;
+        // // EXEMPLO DE OPERADOR TERNÁRIO
+        // int a, b;
 
-        a = 5;
-        b = 6;
+        // a = 5;
+        // b = 6;
 
-        /*
-         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
-         * if(a==b)
-         * resultado = "verdadeiro";
-         * else
-         * resultado = "falso";
-         */
+        // /*
+        // * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        // * if(a==b)
+        // * resultado = "verdadeiro";
+        // * else
+        // * resultado = "falso";
+        // */
 
-        // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
-        String resultado = (a == b) ? "verdadeiro" : "false";
+        // // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL
+        // TERNÁRIO
+        // String resultado = (a == b) ? "verdadeiro" : "false";
 
-        System.out.println(resultado);
+        // System.out.println(resultado);
+
+        // EXEMPLO DE OPERADORES RELACIONAIS
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("Número1 é igual ao Número2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Número1 é diferente do Número2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Número1 é maior do que Número2? " + simNao);
+
+        String nome01 = "Rafael";
+        String nome02 = new String("Rafael");
+
+        System.out.println(nome01 == nome02);
+        System.out.println(nome01.equals(nome02));
     }
 
 }
