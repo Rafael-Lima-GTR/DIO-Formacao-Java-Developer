fc9558aa58a177cfc91b2b9f9df6f06ab3086b97
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-14 16:59:55 -0300

    Criada as variáveis há serem utilizadas no sistema

--- desafios/sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Criada a variável auxiliar para entrada de dados do programa
+        Scanner scEntrada = new Scanner(System.in); 
+        
+        // Criada as variáveis que irão ser trabalhadas no programa ContaTerminal
+        int numeroConta = 0;
+        String numeroAgencia = " ";
+        String nomeCliente = " ";
+        double saldoDaConta = 0;
+
+        System.out.println("Bem Vindo ao Banco DIO!!!");
     }
 }
