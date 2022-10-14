620f4c4882db34755863fc750aac6950f6c0680a
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-14 14:17:55 -0300

    03_Operadores: exemplo 04

--- java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
+++ java-anatomia-classes/src/edu/rafael/sintaxejava/Operadores.java
@@ -42,28 +42,42 @@ public class Operadores {
 
         // System.out.println(resultado);
 
-        // EXEMPLO DE OPERADORES RELACIONAIS
+        // // EXEMPLO DE OPERADORES RELACIONAIS
 
-        int numero1 = 1;
-        int numero2 = 2;
+        // int numero1 = 1;
+        // int numero2 = 2;
 
-        boolean simNao = numero1 == numero2;
+        // boolean simNao = numero1 == numero2;
 
-        System.out.println("Número1 é igual ao Número2? " + simNao);
+        // System.out.println("Número1 é igual ao Número2? " + simNao);
 
-        simNao = numero1 != numero2;
+        // simNao = numero1 != numero2;
 
-        System.out.println("Número1 é diferente do Número2? " + simNao);
+        // System.out.println("Número1 é diferente do Número2? " + simNao);
 
-        simNao = numero1 > numero2;
+        // simNao = numero1 > numero2;
 
-        System.out.println("Número1 é maior do que Número2? " + simNao);
+        // System.out.println("Número1 é maior do que Número2? " + simNao);
 
-        String nome01 = "Rafael";
-        String nome02 = new String("Rafael");
+        // String nome01 = "Rafael";
+        // String nome02 = new String("Rafael");
 
-        System.out.println(nome01 == nome02);
-        System.out.println(nome01.equals(nome02));
+        // System.out.println(nome01 == nome02);
+        // System.out.println(nome01.equals(nome02));
+
+        // EXEMPLO DE OPERADORES LÓGICOS
+        
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
     }
 
 }
