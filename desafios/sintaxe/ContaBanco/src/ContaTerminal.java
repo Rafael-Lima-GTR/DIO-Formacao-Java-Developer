65e3013ad4edeef304ba2a5d433a5e3d9e23eb27
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-10-20 23:05:20 -0300

    Resolução Simulando Uma Conta Bancária Através Do Terminal/Console

--- desafios/sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -2,16 +2,42 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
-        // Criada a variável auxiliar para entrada de dados do programa
-        Scanner scEntrada = new Scanner(System.in); 
-        
-        // Criada as variáveis que irão ser trabalhadas no programa ContaTerminal
-        int numeroConta = 0;
-        String numeroAgencia = " ";
-        String nomeCliente = " ";
-        double saldoDaConta = 0;
 
-        System.out.println("Bem Vindo ao Banco DIO!!!");
+        try (// Criada a variável auxiliar para entrada de dados do programa
+        Scanner scEntrada = new Scanner(System.in)) {
+            // Criada as variáveis que irão ser trabalhadas no programa ContaTerminal
+            int numeroConta = 0;
+            String numeroAgencia = " ";
+            String nomeCliente = " ";
+            double saldoDaConta = 0;
+            String mensagemFinal = "";
+
+            System.out.println("Bem-vindo ao Banco DIO!!!");
+            System.out.println("Por gentileza, digito o número da conta:");
+            numeroConta = scEntrada.nextInt();
+
+            System.out.println("Agora digite o número da Agência Bancária:");
+            numeroAgencia = scEntrada.next();
+            scEntrada.nextLine();
+
+            System.out.println("Digite por gentileza o nome do titular da conta:");
+            nomeCliente = scEntrada.nextLine();
+
+            System.out.println("Por gentileza, insira o valor do saldo em conta:");
+            saldoDaConta = scEntrada.nextDouble();
+
+            mensagemFinal = mensagemFinal.concat("Olá ").concat(nomeCliente)
+                    .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                    .concat(numeroAgencia).concat(", conta ").concat(String.valueOf(numeroConta)).concat(" e seu saldo ")
+                    .concat(String.valueOf(saldoDaConta)).concat(" já está disponível para saque!");
+            System.out.println(mensagemFinal);
+        }
+
+        // Versão 2 utilizando o operador de concatenação "+"
+        // System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em
+        // nosso banco, sua agência é " +
+        // numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoDaConta
+        // + " já está disponível para saque!");
+
     }
 }
